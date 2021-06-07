1bfcc6a69deb004e4e566507a61a26dca6c62f67
abdelnabut <AbdelNabut@outlook.com>
2021-06-06 23:59:40 -0400

    comment

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,12 @@
-package org.example;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 abdel nabut
+ */
 
+
+package org.example;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
